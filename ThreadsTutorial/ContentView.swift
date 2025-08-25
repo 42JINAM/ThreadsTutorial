9580d41d54e0d8ae06664cdda048172ed8134b96
42JINAM <jinam@student.42.fr>
2025-08-25 15:47:45 +0200

    Initial Commit

---
+++ ThreadsTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThreadsTutorial
+//
+//  Created by jinam on 8/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThreadsTutorial/ThreadsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorialApp.swift
+//  ThreadsTutorial
+//
+//  Created by jinam on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
